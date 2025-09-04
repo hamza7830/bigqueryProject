df63411e0bcccd12eca91bf2e6d48c5d6dab62c7
hamza7830 <62361344+hamza7830@users.noreply.github.com>
2025-09-04 13:33:02 +0500

    Delete pipeline.py

--- pipeline.py
+++
@@ -1,6 +0,0 @@
-# from nltk.sentiment import SentimentIntensityAnalyzer
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-# remove all nltk.download / ensure_vader logic entirely
-# analyzer usage stays the same:
-sia = SentimentIntensityAnalyzer()
